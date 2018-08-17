b6d2afa756ded85f4234b4cf17a57a0c35454757
Steven Colburn <stevencolburn@xamdes.com>
2018-08-17 16:48:14 -0700

    add database component for later

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DisplayArticleComponent } from './display-article/display-article.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { DatabaseComponent } from './database/database.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DisplayArticleComponent
+    DisplayArticleComponent,
+    DatabaseComponent
   ],
   imports: [
     BrowserModule,
