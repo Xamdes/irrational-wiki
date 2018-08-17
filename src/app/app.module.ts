bd7f3c9faf1c4144c975cbfc372d3512d4e2143d
Steven Colburn <stevencolburn@xamdes.com>
2018-08-17 14:59:59 -0700

    testing routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DisplayArticleComponent } from './display-article/display-article.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const appRoutes: Routes = [
+  { path: 'article/:title',      component: DisplayArticleComponent },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
@@ -12,6 +17,10 @@ import { DisplayArticleComponent } from './display-article/display-article.compo
     DisplayArticleComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
