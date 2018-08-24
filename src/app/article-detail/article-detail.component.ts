6964871bb960bfe0461ae6dcf9ca9b81cbb9eecd
Steven Colburn <stevencolburn@xamdes.com>
2018-08-24 11:04:27 -0700

    can get url id

--- src/app/article-detail/article-detail.component.ts
+++ src/app/article-detail/article-detail.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { Article } from '../models/article.model';
 
 @Component({
   selector: 'app-article-detail',
   templateUrl: './article-detail.component.html',
   styleUrls: ['./article-detail.component.css']
 })
-export class ArticleDetailComponent implements OnInit {
+export class ArticleDetailComponent implements OnInit
+{
+  articleId: number = null;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private location: Location) { }
 
-  ngOnInit() {
+  ngOnInit()
+  {
+    this.route.params.forEach((urlParameters) =>
+    {
+      this.articleId = parseInt(urlParameters['id']);
+    });  
   }
 
 }
