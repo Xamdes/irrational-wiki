d04f95461a85abdbe9176933ec360375ad812698
Steven Colburn <stevencolburn@xamdes.com>
2018-08-24 12:14:57 -0700

    add service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,7 @@ import { Article } from './models/article.model';
 })
 export class AppComponent {
   title: string = 'IrrationalWiki';
-  articles: Article[] = [
-    new Article('Gravity',0,['This is a test article for a featured article section.','Testing for Paragraph 2','Testing for Paragraph 3']),
-    new Article('Sky',1,['This is a test article for a featured article section.','Testing for Paragraph 2','Testing for Paragraph 3']),
-    new Article('Fire',2,['This is a test article for a featured article section.','Testing for Paragraph 2','Testing for Paragraph 3']),
-    new Article('News Spin',3,['This is a test article for a featured article section.','Testing for Paragraph 2','Testing for Paragraph 3']),
-    new Article('Earth',4,['This is a test article for a featured article section.','Testing for Paragraph 2','Testing for Paragraph 3']),
-    new Article('Mars',5,['This is a test article for a featured article section.','Testing for Paragraph 2','Testing for Paragraph 3'])
-  ];
+  articles: Article[];
 
   getFeaturedArticle()
   {
---
+++ src/app/mock-articles.ts
@@ -0,0 +1,10 @@
+import { Article } from './models/article.model';
+
+export const ARTICLES: Article[] = [
+  new Article('Gravity', 0, ['This is a test article for a featured article section.', 'Testing for Paragraph 2', 'Testing for Paragraph 3']),
+  new Article('Sky', 1, ['This is a test article for a featured article section.', 'Testing for Paragraph 2', 'Testing for Paragraph 3']),
+  new Article('Fire', 2, ['This is a test article for a featured article section.', 'Testing for Paragraph 2', 'Testing for Paragraph 3']),
+  new Article('News Spin', 3, ['This is a test article for a featured article section.', 'Testing for Paragraph 2', 'Testing for Paragraph 3']),
+  new Article('Earth', 4, ['This is a test article for a featured article section.', 'Testing for Paragraph 2', 'Testing for Paragraph 3']),
+  new Article('Mars', 5, ['This is a test article for a featured article section.', 'Testing for Paragraph 2', 'Testing for Paragraph 3'])
+];
