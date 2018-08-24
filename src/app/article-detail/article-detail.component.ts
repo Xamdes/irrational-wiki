ab5d6db192f855d29894542a3a94e6b2b8be7067
Steven Colburn <stevencolburn@xamdes.com>
2018-08-24 16:10:56 -0700

    working route

--- src/app/article-detail/article-detail.component.ts
+++ src/app/article-detail/article-detail.component.ts
@@ -2,9 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 import { Article } from '../models/article.model';
-import { ArticleService } from '../article.service';
-import { FirebaseListObservable } from 'angularfire2/database';
-import { FirebaseObjectObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-article-detail',
@@ -14,20 +11,17 @@ import { FirebaseObjectObservable } from 'angularfire2/database';
 export class ArticleDetailComponent implements OnInit
 {
   articleId: string = "";
-  articles: FirebaseListObservable<any[]>;
-  articleToDisplay: Article;
 
-  constructor(private route: ActivatedRoute, private location: Location, private articleService: ArticleService) { }
+
+  constructor(private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit()
   {
-    this.articles = this.articleService.getArticles();
     this.route.params.forEach((urlParameters) =>
     {
       this.articleId = urlParameters['id'];
     });
     console.log(this.articleId);
-    console.log(this.articles)
   }
 
 }
