1b5287377566a002da7ee5972ec720fe34ba4d34
Steven Colburn <stevencolburn@xamdes.com>
2018-08-31 13:51:42 -0700

    setup add article front end

--- src/app/new-article/new-article.component.ts
+++ src/app/new-article/new-article.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AppComponent } from '../app.component';
+import { Article } from '../models/article.model';
+import { routing } from '../app.routing';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ArticleService } from '../services/article.service';
+import { FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-new-article',
   templateUrl: './new-article.component.html',
-  styleUrls: ['./new-article.component.css']
+  styleUrls: ['./new-article.component.css'],
+  providers: [ArticleService]
 })
-export class NewArticleComponent implements OnInit {
+export class NewArticleComponent {
 
-  constructor() { }
+  constructor(private router: Router, private articleService: ArticleService, private route: ActivatedRoute, private location: Location) { }
 
-  ngOnInit() {
+  addArticle(author: string, title: string, paragraphs: string)
+  {
+
   }
 
 }
